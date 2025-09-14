0ff5271cfcbf85c1739687420f918f9d6679bfff
GustavoSilva <Gustavo_Silva@McKinsey.com>
2025-09-14 17:40:58 -0300

    Initial Commit

---
+++ src/test/java/com/clients/clients_management/ClientsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clients.clients_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
